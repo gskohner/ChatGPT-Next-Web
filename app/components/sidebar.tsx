ce898628d2f08249c495063c86f13a54950aa319
gskohner <gskohner@gmail.com>
2023-05-03 10:49:56 -0700

    change to code hero

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,9 +96,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>Code Hero</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Your superhuman Salesforce Developer Assistant
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
